eb63bd10f473094b922d84c58bc2349ca88e2156
kuniyoshi9244 <kuniyoshi.koichiro.345611@gmail.com>
2023-04-14 15:15:23 +0900

    add process function

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -17,7 +17,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -45,5 +47,16 @@ defmodule Issues.CLI do
 
   def args_to_internal_representation(_) do
     :help
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user><project> [count | #{@default_count}]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    #Issues.GithubIssues(user, project)
   end
 end
