1fcb0df52d6ec45566c61dcee060c5bcfbdd9371
kuniyoshi9244 <kuniyoshi.koichiro.345611@gmail.com>
2023-04-14 17:08:23 +0900

    extract URL to config file

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,6 +1,8 @@
 defmodule Issues.GithubIssues do
   @user_agent [ {"User-agent", "Elixir dava@pragprog.com"} ]
 
+  @github_url Application.get_env(:issues, :github_url)
+
   def fetch(user, project) do
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
@@ -8,7 +10,7 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({ :ok, %{status_code: status_code, body: body}}) do
